b1a60c31df3ef7a82fedde354774bc1982edac20
Mina <minatari@uw.edu>
2015-12-03 16:50:33 -0800

    middle of class 12/3

--- web-service/server.js
+++ web-service/server.js
@@ -3,3 +3,93 @@
     main server script for our task list web service
 */
 
+var port = 8080;
+
+//load all modules we need
+//express web server framework
+var express = require('express');
+
+//sqlite library
+var sqlite = require('sqlite3');
+
+//body parser library
+var bodyParser = require('body-parser');
+
+//create a new express app
+var app = express();
+
+//swiss army knife
+//lets us read anything in static subdirectory (css, js), not above
+//tell express to serve static files from the /static subdir
+app.use(express.static(__dirname + '/static'));
+
+//tell express to parse post body data as json
+app.use(bodyParser.json());
+
+//add a route for our home page
+app.get('/api/tasks', function(req, res, next) {
+    //dont get any tasks already completed
+    //rowid is automatically created w/ sqlite, primary key
+    var sql = 'select rowid,title,done,createdOn from tasks where done != 1';
+    db.all(sql, function(err, rows) {
+        if (err) {
+            return next(err);
+        }
+        //
+        res.json(rows);
+    });
+});
+
+//when someone posts to /api/tasks...
+app.post('/api/tasks', function(req, res, next) {
+    var newTask = {
+        title: req.body.title,
+        done: false,
+        createdOn: new Date()
+    };
+    var sql = 'insert into tasks(title,done,createdOn) values(?,?,?)';
+    db.run(sql, [newTask.title, newTask.done, newTask.createdOn], function(err) {
+        if (err) {
+            return next(err);
+        }
+
+        //201 is http status code that everything is okay + something got created successfully
+        res.status(201).json(newTask);
+    });
+});
+
+//when someone PUTs to /api/tasks/<task-id>...
+app.put('/api/tasks/:rowid', function(req, res, next) {
+    var sql = 'update tasks set done=? where rowid=?';
+    db.run(Sql, [red.body.done, req.params.rowid], function(err) {
+        if (err) {
+            return next(err);
+        }
+
+        res.json(req.body);
+
+    });
+    //DONT DO THIS. SHAAAAME!!!
+    //var sql = 'update tasks set done' + req.body
+
+});
+
+//create database
+var db = new sqlite.Database(__dirname + '/data/tasks.db', function(err) {
+    if (err) {
+        throw err;
+    }
+
+    var sql = 'create table if not exists ' +
+        'tasks(title string, done int, createdOn datetime)';
+    db.run(sql, function(err) {
+        if (err) {
+            throw err;
+        }
+    });
+
+    //start the server
+    app.listen(port, function() {
+        console.log('server is listening on http://localhost:' + port);
+    });
+});
